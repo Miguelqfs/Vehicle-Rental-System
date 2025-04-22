b9a3b90db8960581bc4e7c3cb8ba1015f465e7fc
Miguel Queiroz <mqfs@academico.ufpb.br>
2025-04-22 09:04:46 -0300

    refactor: adicionando novos atributos, getters e setters às classes

--- Coletivo.java
+++ Coletivo.java
@@ -1,13 +1,34 @@
 public class Coletivo extends Veiculo {
+    private int portas;
+    private boolean banheiro;
 
-    public Coletivo(int id, String placa, int capacidade, boolean alugado, int ano) {
+    public Coletivo(int id, String placa, int capacidade, boolean alugado, int ano, int portas, boolean banheiro) {
         super(id, placa, capacidade, alugado, ano);
+        this.portas = portas;
+        this.banheiro = banheiro;
+    }
+
+    public int getPortas() {
+        return portas;
+    }
+
+    public void setPortas(int portas) {
+        this.portas = portas;
+    }
+
+    public boolean temBanheiro() {
+        return banheiro;
+    }
+
+    public void setBanheiro(boolean banheiro) {
+        this.banheiro = banheiro;
     }
 
     @Override
     public void exibirInformacoes() {
         System.out.println("Coletivo - ID: " + getId() + ", Placa: " + getPlaca() + 
                            ", Capacidade: " + getCapacidade() + ", Ano: " + getAno() + 
-                           ", Alugado: " + isAlugado());
+                           ", Alugado: " + isAlugado() + ", Portas: " + portas +
+                           ", Banheiro: " + (banheiro ? "Sim" : "Não"));
     }
 }
